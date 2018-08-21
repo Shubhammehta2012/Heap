faf895a0317d46687d1bc7f5cf34a3aff8e52894
Shubham Mehta <31146862+Shubhammehta2012@users.noreply.github.com>
2018-08-21 02:14:52 -0700

    Update HeapSort.py

--- HeapSort.py
+++ HeapSort.py
@@ -1,19 +1,3 @@
-# Author: OMKAR PATHAK
-
-#  Approach:
-#  Heap sort happens in two phases. In the first phase, the array
-#  is transformed into a heap. A heap is a binary tree where
-#  1) each node is greater than each of its children
-#  2) the tree is perfectly balanced
-#  3) all leaves are in the leftmost position available.
-#  In phase two the heap is continuously reduced to a sorted array:
-#  1) while the heap is not empty
-#  - remove the top of the head into an array
-#  - fix the heap.
-
-#  Time Complexity of Solution:
-#  Best O(nlog(n)); Average O(nlog(n)); Worst O(nlog(n)).
-
 def HeapSort(alist):
     heapify(alist)              # create the heap
     end = len(alist) - 1
@@ -23,8 +7,6 @@ def HeapSort(alist):
         end -= 1
 
 def heapify(alist):
-    ''' This function helps to maintain the heap property '''
-    # start = (len(alist) - 2) // 2         (faster execution)
     start = len(alist) // 2
     while start >= 0:
         shiftDown(alist, start, len(alist) - 1)
